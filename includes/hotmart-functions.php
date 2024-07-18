7e32550eae0fae6a2cb2c2b832c910a2335bd90f
FilipeBarcellos <brad_lipp@hotmail.com>
2024-07-18 01:11:21 -0300

    Update hotmart-functions.php

--- includes/hotmart-functions.php
+++ includes/hotmart-functions.php
@@ -5,7 +5,7 @@
  * @param string $full_name Nome completo.
  * @return array Array com o primeiro e último nome.
  */
-function hotmart_split_full_name($full_name) {
+function hotmart_split_full_name($full_name) { // Novo nome da função
     $parts = explode(' ', $full_name);
     $last_name = array_pop($parts);
     $first_name = implode(' ', $parts);
