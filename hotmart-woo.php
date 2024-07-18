4bf94d489985ccf0bb9fe0708660bed7e1c2a21a
FilipeBarcellos <brad_lipp@hotmail.com>
2024-07-17 22:51:34 -0300

    Update hotmart-woo.php

--- hotmart-woo.php
+++ hotmart-woo.php
@@ -1,1 +1,19 @@
+<?php
+/*
+Plugin Name: WooCommerce - Integrador de compras Hotmart
+Description: Plugin para integrar as compras feitas na Hotmart com o WooCommerce.
+Author: Filipe Barcellos
+Version: 1.0
+*/
 
+// Inclui os arquivos das classes
+require_once plugin_dir_path(__FILE__) . 'includes/class-hotmart-webhook.php';
+require_once plugin_dir_path(__FILE__) . 'includes/class-hotmart-emails.php';
+require_once plugin_dir_path(__FILE__) . 'includes/class-hotmart-woocommerce.php';
+require_once plugin_dir_path(__FILE__) . 'includes/class-hotmart-wordpress.php';
+
+// Inicializa as classes
+new Hotmart_Webhook();
+new Hotmart_Emails();
+new Hotmart_WooCommerce();
+new Hotmart_WordPress();
