eaa67adeb0a13f3282c5a384120b45e88512ee37
FilipeBarcellos <brad_lipp@hotmail.com>
2024-07-18 03:20:02 -0300

    Update hotmart-e-woocommerce-em-pastas-main.php

--- hotmart-e-woocommerce-em-pastas-main.php
+++ hotmart-e-woocommerce-em-pastas-main.php
@@ -9,12 +9,14 @@ Version: 1.0
 // Define a constante para o caminho da pasta includes
 define('HOTMART_PLUGIN_INCLUDES_DIR', plugin_dir_path(__FILE__) . 'includes/');
 
-// Inclui os arquivos das classes do plugin (nessa ordem)
-require_once HOTMART_PLUGIN_INCLUDES_DIR . 'hotmart-functions.php';  // Primeiro
-require_once HOTMART_PLUGIN_INCLUDES_DIR . 'class-hotmart-emails.php';   
-require_once HOTMART_PLUGIN_INCLUDES_DIR . 'class-hotmart-woocommerce.php'; 
+// Inclui o arquivo de funções auxiliares
+require_once HOTMART_PLUGIN_INCLUDES_DIR . 'hotmart-functions.php';
+
+// Inclui os arquivos das classes do plugin
+require_once HOTMART_PLUGIN_INCLUDES_DIR . 'class-hotmart-webhook.php';
+require_once HOTMART_PLUGIN_INCLUDES_DIR . 'class-hotmart-emails.php';
+require_once HOTMART_PLUGIN_INCLUDES_DIR . 'class-hotmart-woocommerce.php';
 require_once HOTMART_PLUGIN_INCLUDES_DIR . 'class-hotmart-wordpress.php';
-require_once HOTMART_PLUGIN_INCLUDES_DIR . 'class-hotmart-webhook.php'; // Por último
 
 // Inicializa as classes
 new Hotmart_Webhook();
