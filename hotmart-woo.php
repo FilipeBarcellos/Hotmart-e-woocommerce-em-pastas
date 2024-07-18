58d4c6d9103bbe7707b0936caa92ab6a66ed8ed3
FilipeBarcellos <brad_lipp@hotmail.com>
2024-07-18 00:10:11 -0300

    Update hotmart-woo.php

--- hotmart-woo.php
+++ hotmart-woo.php
@@ -7,17 +7,13 @@
  */
 
 // Inclui os arquivos das classes do plugin.
-// Cada classe é responsável por uma parte específica da funcionalidade do plugin.
-require_once plugin_dir_path(__FILE__) . 'includes/class-hotmart-webhook.php'; // Lida com o recebimento e processamento de webhooks da Hotmart.
-require_once plugin_dir_path(__FILE__) . 'includes/class-hotmart-emails.php';   // Gerencia o envio de e-mails relacionados às compras.
-require_once plugin_dir_path(__FILE__) . 'includes/class-hotmart-woocommerce.php'; // Realiza a integração com o WooCommerce (criação de pedidos, clientes, etc.).
-require_once plugin_dir_path(__FILE__) . 'includes/class-hotmart-wordpress.php';  // Lida com as configurações do plugin no painel do WordPress.
-require_once plugin_dir_path(__FILE__) . 'includes/hotmart-functions.php';
-
-$hotmart_emails = new Hotmart_Emails(); // Cria uma instância global da classe
+require_once plugin_dir_path(__FILE__) . 'includes/class-hotmart-webhook.php';
+require_once plugin_dir_path(__FILE__) . 'includes/class-hotmart-emails.php';
+require_once plugin_dir_path(__FILE__) . 'includes/class-hotmart-woocommerce.php';
+require_once plugin_dir_path(__FILE__) . 'includes/class-hotmart-wordpress.php';
+require_once plugin_dir_path(__FILE__) . 'includes/hotmart-functions.php'; // Inclui o arquivo de funções auxiliares
 
 // Cria instâncias das classes do plugin.
-// Isso permite que as funções e métodos das classes sejam utilizados.
 new Hotmart_Webhook();
 new Hotmart_Emails();
 new Hotmart_WooCommerce();
