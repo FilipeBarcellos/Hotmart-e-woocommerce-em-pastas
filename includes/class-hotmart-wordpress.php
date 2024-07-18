125d6e338ed84fe7a219eb3b040b831d38a61b46
FilipeBarcellos <brad_lipp@hotmail.com>
2024-07-18 00:37:37 -0300

    Update class-hotmart-wordpress.php

--- includes/class-hotmart-wordpress.php
+++ includes/class-hotmart-wordpress.php
@@ -1,5 +1,5 @@
 <?php
-require_once plugin_dir_path(dirname(__FILE__)) . 'hotmart-functions.php'; // Inclusão do arquivo de funções auxiliares
+require_once HOTMART_PLUGIN_INCLUDES_DIR . 'hotmart-functions.php';
 
 /**
  * Classe responsável pelas configurações do plugin no painel do WordPress.
