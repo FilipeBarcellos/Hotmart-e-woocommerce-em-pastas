1fb4ef32e82be618ee9ace1493e0905792e8eb58
FilipeBarcellos <brad_lipp@hotmail.com>
2024-07-20 12:36:10 -0300

    Update hotmart-e-woocommerce-em-pastas-main.php

--- hotmart-e-woocommerce-em-pastas-main.php
+++ hotmart-e-woocommerce-em-pastas-main.php
@@ -14,11 +14,10 @@ require_once HOTMART_PLUGIN_INCLUDES_DIR . 'hotmart-functions.php';
 
 // Função hotmart_log_error movida para o arquivo principal
 function hotmart_log_error($error_message, $error_data = array()) {
-$log_file = WP_CONTENT_DIR . '/hotmart.log';
-$log_enabled = get_option('hotmart_logging_enabled', 'no') === 'yes';
-$log_raw_data = get_option('hotmart_log_raw_data', 'no') === 'yes';
+    $log_file = WP_CONTENT_DIR . '/hotmart.log';
+    $log_enabled = get_option('hotmart_logging_enabled', 'no') === 'yes';
 
-if ($log_enabled) {
+    if ($log_enabled) {
 // Verifica se o diretório do log existe, se não, tenta criar
 $log_dir = dirname($log_file);
 if (!is_dir($log_dir)) {
@@ -31,25 +30,20 @@ error_log("Hotmart Error: Unable to write to log file ($log_file).");
 return;
 }
 
-$message = date('Y-m-d H:i:s') . ' - ' . $error_message . ' - ';
-if ($log_raw_data) {
-$message .= esc_html(print_r($error_data, true)) . "\n"; // Sanitização dos dados
-} else {
-$message .= json_encode($error_data) . "\n";
+        $message = date('Y-m-d H:i:s') . ' - ' . $error_message . ' - ';
+        $message .= json_encode($error_data) . "\n"; // Registra os dados do erro em JSON
+        error_log($message, 3, $log_file);
+
+        // Chamar a função de envio de e-mail (opcional)
+        $hotmart_emails = new Hotmart_Emails();
+        $hotmart_emails->hotmart_send_error_email($error_message, $error_data);
+    }
+
+    // Chama a função para registrar os dados brutos (se habilitado)
+    $hotmart_wordpress = new Hotmart_WordPress();
+    $hotmart_wordpress->hotmart_log_raw_data($error_data); // Passa os dados brutos para a nova função
 }
 
-// Tenta escrever no arquivo de log, tratando possíveis erros
-try {
-error_log($message, 3, $log_file);
-
-// Chamar a função de envio de e-mail
-$hotmart_emails = new Hotmart_Emails();
-$hotmart_emails->hotmart_send_error_email($error_message, $error_data);
-} catch (Exception $e) {
-error_log("Hotmart Error: Failed to write to log file. " . $e->getMessage());
-}
-}
-}
 
 
 // Inclui os arquivos das classes do plugin
