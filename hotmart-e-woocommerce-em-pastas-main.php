fdf073b6f43feb934a4843b47fb106938c4741f8
FilipeBarcellos <brad_lipp@hotmail.com>
2024-07-20 02:05:00 -0300

    Update hotmart-e-woocommerce-em-pastas-main.php

--- hotmart-e-woocommerce-em-pastas-main.php
+++ hotmart-e-woocommerce-em-pastas-main.php
@@ -14,27 +14,43 @@ require_once HOTMART_PLUGIN_INCLUDES_DIR . 'hotmart-functions.php';
 
 // Função hotmart_log_error movida para o arquivo principal
 function hotmart_log_error($error_message, $error_data = array()) {
-    $log_file = WP_CONTENT_DIR . '/hotmart.log';
-    $log_enabled = get_option('hotmart_logging_enabled', 'no') === 'yes';
-    $log_raw_data = get_option('hotmart_log_raw_data', 'no') === 'yes';
+$log_file = WP_CONTENT_DIR . '/hotmart.log';
+$log_enabled = get_option('hotmart_logging_enabled', 'no') === 'yes';
+$log_raw_data = get_option('hotmart_log_raw_data', 'no') === 'yes';
 
-    if ($log_enabled) {
-        $message = date('Y-m-d H:i:s') . ' - ' . $error_message . ' - ';
-        if ($log_raw_data) {
-            $message .= print_r($error_data, true) . "\n";
-        } else {
-            $message .= json_encode($error_data) . "\n";
-        }
-        error_log($message, 3, $log_file);
+if ($log_enabled) {
+// Verifica se o diretório do log existe, se não, tenta criar
+$log_dir = dirname($log_file);
+if (!is_dir($log_dir)) {
+mkdir($log_dir, 0755, true);
+}
 
-        // Chamar a função de envio de e-mail
-    // Cria uma instância da classe Hotmart_Emails
-    $hotmart_emails = new Hotmart_Emails();
+// Verifica se o arquivo de log existe e é gravável
+if (!file_exists($log_file) || !is_writable($log_file)) {
+error_log("Hotmart Error: Unable to write to log file ($log_file).");
+return;
+}
 
-    // Chama o método de envio de e-mail através da instância
-    $hotmart_emails->hotmart_send_error_email($error_message, $error_data);
+$message = date('Y-m-d H:i:s') . ' - ' . $error_message . ' - ';
+if ($log_raw_data) {
+$message .= esc_html(print_r($error_data, true)) . "\n"; // Sanitização dos dados
+} else {
+$message .= json_encode($error_data) . "\n";
+}
+
+// Tenta escrever no arquivo de log, tratando possíveis erros
+try {
+error_log($message, 3, $log_file);
+
+// Chamar a função de envio de e-mail
+$hotmart_emails = new Hotmart_Emails();
+$hotmart_emails->hotmart_send_error_email($error_message, $error_data);
+} catch (Exception $e) {
+error_log("Hotmart Error: Failed to write to log file. " . $e->getMessage());
 }
 }
+}
+
 
 // Inclui os arquivos das classes do plugin
 require_once HOTMART_PLUGIN_INCLUDES_DIR . 'class-hotmart-wordpress.php';
